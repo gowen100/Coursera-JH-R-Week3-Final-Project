836bf1e48022576093783adc4e8870e9855ea16d
Gareth <MY_NAME@example.com>
2020-08-19 19:13:01 +0100

    Added in the averaging and writing to a table

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,19 @@
-
-
-
 # Import the dplyr library
-
-
 library(dplyr)
-#########################################################################
-# 1.first function loads the x,y and subject tables from the UCI HAR Dataset directory and combines them to produce a combined set
 
-loadCombineData <- function () {
+##########################################################################
+# Code to load and clean up smartphone data from from the UCI HAR Dataset#
+# loads in the data from the local directory "UCI HAR Dataset" then      #
+# combines and averages the mean and std dev observations and outputs    #
+# with cleaner titles and better activity labels.                        #
+#                                                                        #
+##########################################################################
+
+
+# first function loads the x,y and subject tables directory and combines them to produce a combined set
+
+
+loadCombineOutputData <- function () {
 #define the column headings. Read in the features from the feature file
 x.labels <- read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
 y.labels <- "activity"
@@ -33,24 +38,34 @@ combined <- rbind(test,train)
 
 # Clear the original files from the workspace
 remove(subject.test, x.test, y.test, subject.train,x.train, y.train, test, train,x.labels,subject.labels,y.labels)
-}
 
-#########################################################################
-# 2.second function cleans up a lot of the data
+#change the numerical activity labels to more meaningful labels by loading a conversion table and merging
 
 #pull in the activity labels
 activity.labels <- read.csv("UCI HAR Dataset/activity_labels.txt",col.names = c("activity","activitylabel"),
                             sep="", header=FALSE)
-
 labelcombined <- merge(combined,activity.labels)
 
-#remove columns that do not contain mean or std
+#remove columns that do not contain mean or std and drop the numeric activity label
 data <- select(labelcombined,"subject","activitylabel",grep("(mean|std)",names(combined),value=TRUE)) 
 
 #clean up the workspace
 remove(combined,activity.labels,labelcombined)
 
-data.subject <- data %>%
-  group_by(subject) %>%
+#get the means for each subject and activity
+data.mean <- data %>%
+  group_by(subject,activitylabel) %>%
   summarise_all(mean)
+
+#get the column headings and clean them up
+names <- names(data.mean)
+namesNew <- gsub("(^f|^t)","",names)
+namesNew <- gsub("\\.","",namesNew)
+namesNew <- tolower(namesNew)
+namesNew
+
+#replace the column names with clearer explanations
+colnames(data.mean) <-namesNew
+write.table(data.mean, file="data_summary.txt", row.name=FALSE)
+}
 
